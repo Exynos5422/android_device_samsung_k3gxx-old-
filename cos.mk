ad04d5b34b2a5086efdf312326b3d7d661033d82
aditheking <aupreti72@gmail.com>
2016-12-18 05:05:49 +0000

    Cos-ify

--- cos.mk
+++ cos.mk
@@ -1,8 +1,8 @@
 # Release name
 PRODUCT_RELEASE_NAME := k3gxx
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/common.mk)
 
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
@@ -10,8 +10,10 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, device/samsung/k3gxx/device_k3gxx.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_k3gxx
+PRODUCT_NAME := cos_k3gxx
 PRODUCT_DEVICE := k3gxx
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL=SM-G900H
 PRODUCT_MANUFACTURER := samsung
+
+COS_RELEASE = true
